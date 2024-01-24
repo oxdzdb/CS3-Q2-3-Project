ba2ae52f5ad1f51d4e9f8e4a8d69384d3eb77482
Alex Empedrad <b2027jaempedrad@pshs.edu.ph>
2024-01-24 22:31:30 +0800

    message

--- script.js
+++ script.js
@@ -1,4 +1,16 @@
+
+
 /* 
   do not forget to add comments and follow good coding standards 
 */
+
+function bitcoinGraph(value) {
+	
+	if (value == "bitcoin") {
+		document.getElementById("cryptocurrency").innerHTML = '<img id="cryptoImage" src="Images/cryptoImage.png">';
+	}
+	else {
+		document.getElementById("cryptocurrency").innerHTML = " ";
+	}
+}
 
