04292a36eafd303bb00e9b8a89eaf66fec18f181
Alex Empedrad <b2027jaempedrad@pshs.edu.ph>
2024-01-24 23:18:29 +0800

    Update 3: Cryptotool

--- script.js
+++ script.js
@@ -8,7 +8,8 @@ function bitcoinGraph(value) {
 	
 	if (value == "bitcoin") {
 		document.getElementById("cryptocurrency").innerHTML = '<img id="cryptoImage" src="Images/cryptoImage.png" <br> <br> <h1> 1 Bitcoin = 2,238,287.42 Philippine Peso </h1>' +
-		'<br> <h2 id="cc-text"> Input value: </h2> <input id="ccinput" type="number"> <button onclick="bcToPhp(Number(ccinput.value))"> Enter </button> ';
+		'<br> <h2 id="cc-text"> Input value: </h2> <input id="ccinput" type="number"> <button onclick="bcToPhp(Number(ccinput.value))"> Enter </button> ' + 
+		'<br> <h2> In Philippine Peso: </h2>';
 	}
 	else {
 		document.getElementById("cryptocurrency").innerHTML = " ";
