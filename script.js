4162a19be025ce55a0253e3744be8a54297b33ce
oxidized <oxidizedb@gmail.com>
2024-02-21 19:48:25 +0800

    shopping revamp

--- script.js
+++ script.js
@@ -23,9 +23,15 @@ function price(x , y){
 }
 
 function cartDisplay(){
-	let x = document.getElementById("bdgQuant").value
+	let amt = document.getElementById("amtQuant").value
+	let clc = document.getElementById("clcQuant").value
+	let pcb = document.getElementById("pcbQuant").value
+	let mmg = document.getElementById("mmgQuant").value
+	let aki = document.getElementById("akiQuant").value
+	let bdg = document.getElementById("bdgQuant").value
+	let totalQuant = amt + clc + pcb + mmg + aki + bdg;
 	
-	document.getElementById("inputInfo").innerHTML = "Quantity: " + x + "<br>" + "Price: " + 39.99*x;
+	document.getElementById("inputInfo").innerHTML = "Quantity: " + totalQuant + "<br>" + "Price: " + (149.99*amt)+(199.99*clc)+(129.99*pcb)+(169.99*mmg)+(189.99*aki)+(39.99*bdg);
 }
 
 function swapStyleSheet(sheet) {
