e37e1a2bd34f7ab00570d9d9dae797d96fd98a86
oxidized <oxidizedb@gmail.com>
2024-02-21 18:20:59 +0800

    up-to-date

--- script.js
+++ script.js
@@ -1,12 +1,13 @@
-let total = 0;
-
 function bitcoinGraph(value) {
 
   if (value == "bitcoin") {
-    document.getElementById("cryptocurrency").innerHTML = '<img id="cryptoImage" src="images/cryptoImage.png"> <br> <br> <h1> 1 Bitcoin = 2,238,287.42 Philippine Peso </h1> <br> <h2 id="cc-text"> Input value: </h2> <input id="ccinput" type="number"> <button onclick="bcToPhp(Number(ccinput.value))"> Enter </button> <br> <h2> In Philippine Peso: </h2>';
+    document.getElementById("cryptocurrency").innerHTML = '<img id="cryptoImage" src="images/cryptoImage.png">' + 
+      '<br> <br> <h1> 1 Bitcoin = 2,238,287.42 Philippine Peso </h1> <br> ' + 
+      '<h2 id="cc-text"> Input value: </h2> <input id="ccinput" type="number"> ' +
+      '<button onclick="bcToPhp(Number(ccinput.value))"> Enter </button> <br> <h2> In Philippine Peso: </h2>';
   }
   else {
-    document.getElementById("cryptocurrency").innerHTML = " ";
+    document.getElementById("cryptocurrency").innerHTML = '  ';
   }
 }
 
@@ -14,10 +15,10 @@ function bcToPhp(value) {
   document.getElementById("ccoutput").innerHTML =  value*2238287.42 + ' Php' ;
 }
 
-function cashRegister() {
-	document.getElementById("totalPrice").innerHTML = total;
+function price(x , y){
+  
+  alert(x + y + " is a great choice!");
 }
 
-function priceTotal() {
-	total = total + 149.99;
-}
+
+
